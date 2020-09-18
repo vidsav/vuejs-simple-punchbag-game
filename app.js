071c96877f49e7e9c2c8b4a748f24102854ac47e
Vid <savnikvid5@gmail.com>
2020-09-18 22:58:02 +0200

    functions and click events

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 new Vue({
     el: '#vue-app',
     data: {
-        
+        health: 100,
+        ended: false
     },
+    methods: {
+        punch: function(){
+            this.health-= 10;
+        },
+        restart: function(){
+            this.health = 100;
+        }
+    },
+    computed: {
+
+    }
 });

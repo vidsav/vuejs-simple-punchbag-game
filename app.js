f53a44d217ef522ae3adf8719c66f555fbcfe5c7
Vid <savnikvid5@gmail.com>
2020-09-18 23:09:58 +0200

    finishing

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ new Vue({
     methods: {
         punch: function(){
             this.health-= 10;
+            if(this.health <= 0){
+                this.ended = true;
+            }
         },
         restart: function(){
             this.health = 100;
+            this.ended = false;
         }
     },
     computed: {
